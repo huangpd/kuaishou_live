2545b256484489e0a70b14be0e93cdf86b202655
huangpd <1655979823@qq.com>
2024-06-21 19:04:28 +0800

    Update browse_token.py

--- browse_token.py
+++ browse_token.py
@@ -8,8 +8,6 @@ index = 0
 
 def get_live_info(eid):
     co = ChromiumOptions()
-    # co.set_user_agent(
-    #     'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1')
     co.set_user_agent(
         'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1')
     # co.set_proxy('ip:port') #设置代理非常重要
@@ -17,10 +15,8 @@ def get_live_info(eid):
     co.incognito(True)
     # co.auto_port()
     page = ChromiumPage(addr_or_opts=co)
-
     try:
         page.listen.start('livev.m.chenzhongtech.com/rest/k/live/byUser?kpn=GAME_ZONE&kpf=')
-
         page.get(f'https://live.kuaishou.com/u/{eid}')
         time.sleep(3)
         # print(page.html)
@@ -29,25 +25,17 @@ def get_live_info(eid):
             page.close()
             page.quit()
             while True:
-
                 global index
-                print(index)
-
                 index += 1
                 if index == 6:
                     break
                 return get_live_info(eid)
-
         for _ in range(2):
-            # page.refresh()
             if page.url == f'https://live.kuaishou.com/u/{eid}' or 'captcha' in page.url:
                 page.quit()
                 return get_live_info(eid)
-
             res = page.listen.wait()
             time.sleep(2)
-            # print(res)
-            # print(res.response.body)
             if res.response.body:
                 data = res.response.body
                 print(data)
@@ -55,13 +43,11 @@ def get_live_info(eid):
                     page.close()
                     page.quit()
                     return get_live_info(eid)
-
                 liveStreamId = data['liveStream']['liveStreamId']
                 kwaiId = data['liveStream']['userEid']
                 token = data['token']
                 webSocketAddresses = data['webSocketAddresses'][0]
                 page.quit()
                 return liveStreamId, kwaiId, token, webSocketAddresses
-
     except:
         page.quit()
